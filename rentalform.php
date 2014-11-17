f05e393a54de1646ac935398d86057b630ab039a
Courtney Hendricks <courthendricks@gmail.com>
2014-11-17 12:17:27 -0800

    Creates php file for submitting form info, Adds form validation, Updates all pages with responsive-friendly .top-section

---
+++ rentalform.php
@@ -0,0 +1,58 @@
+<?php
+if (isset($_POST['service'])) {
+    $service = $_POST['service'];
+    // $service is an array of selected values
+}
+
+
+$Name = Trim(stripslashes($_POST['Name'])); 
+$Phone = Trim(stripslashes($_POST['Phone'])); 
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+
+$EmailFrom = $Email;
+$EmailTo = "courthendricks@gmail.com";
+$Subject = "Rental Request Form";
+
+
+
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "\n";
+$Body .= "Phone: ";
+$Body .= $Phone;
+$Body .= "\n";
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+$Body .= "\n";
+
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=rentalsuccess.html\">";	
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=404.html\">";
+}
+?>
