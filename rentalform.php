be1ddfb2899de011e4b4513578eb6932162d46f9
Courtney Hendricks <courthendricks@gmail.com>
2014-12-18 19:25:38 -0800

    Fixes Accordion: Tab snaps to top of page, fixes entertainment tabs and tables

--- rentalform.php
+++ rentalform.php
@@ -15,9 +15,6 @@ $EmailFrom = $Email;
 $EmailTo = "courthendricks@gmail.com";
 $Subject = "Fairgrounds Rental Request Form";
 
-$headers .='X-Mailer: PHP/' . phpversion();
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
 
 
 // validation
